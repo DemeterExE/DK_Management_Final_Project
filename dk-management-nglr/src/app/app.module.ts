a6251416a9183d70371870318ea19ac484487d3e
DemeterExE <demeterexe2@gmail.com>
2024-06-26 00:04:05 +0300

    few styling changes

--- dk-management-nglr/src/app/app.module.ts
+++ dk-management-nglr/src/app/app.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { OrdersComponent } from './components/orders/orders.component';
-import { AppRoutingModule, routes } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CustomerService } from './services/customer.service';
 import { OrderService } from './services/order.service';
 import { CommonModule } from '@angular/common';
